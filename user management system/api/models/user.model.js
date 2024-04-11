59b7f7fed26bc5b1a3ca920fbb8fed1d646e90da
SamindiS <samindisankalpani@gmail.com>
2024-04-11 20:09:25 +0530

    admin signup

--- user management system/api/models/user.model.js
+++ user management system/api/models/user.model.js
@@ -19,6 +19,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: 'https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg',
     },
+    userType: {
+        type: String,
+
+    },
+    secretKey: {
+        type: String,
+
+    },
+
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
--- user management system/client/src/pages/SignUp.jsx
+++ user management system/client/src/pages/SignUp.jsx
@@ -7,12 +7,19 @@ export default function SignUp() {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const [userType, setUserType] = useState("");
+  const [sacretKey, setSacretKey] = useState("");
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+
+    if(userType=="Admin" && sacretKey!=="Admin@123"){
+      alert("Invalid Admin! Please enter correct secret key");
+    }else{
+
+      e.preventDefault();
     try {
       setLoading(true);
       setError(false);
@@ -35,11 +42,35 @@ export default function SignUp() {
       setLoading(false);
       setError(true);
     }
+    }
+    
   };
   return (
+     
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      
+      <div className="flex justify-end items-center gap-2">
+        <span>Register as</span>
+        <input
+          type="radio"
+          id="admin"
+          name="userType"
+          value="Admin"
+          onChange={(e) => setUserType(e.target.value)}
+        />
+       Admin
+      </div>
+     <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        {userType==="Admin" ?(
+      <input
+          type="text"
+          placeholder="Secret Key"
+          id="sacretKey"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={(e)=>setSacretKey(e.target.value)}
+        />
+        ):null}
         <input
           type="text"
           placeholder="Username"

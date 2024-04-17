8ef9cecfacf6911076c960ddf7232a4b69a716dd
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2024-04-17 14:26:00 +0530

    report generate

--- frontend/src/components/AllMaterials.js
+++ frontend/src/components/AllMaterials.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useReactToPrint } from "react-to-print";
 
 export default function AllMaterials() {
   const [materials, setMaterials] = useState([]);
@@ -9,7 +10,7 @@ export default function AllMaterials() {
       axios
         .get("http://localhost:8070/material/")
         .then((res) => {
-            setMaterials(res.data);
+          setMaterials(res.data);
           // console.log(res)
         })
         .catch((err) => {
@@ -18,7 +19,6 @@ export default function AllMaterials() {
     }
     getMaterials();
   }, []);
-
 
   // delete function
   const handleDelete = async (materialId) => {
@@ -33,18 +33,51 @@ export default function AllMaterials() {
     }
   };
 
+  //report generate
+  const ComponentsRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => ComponentsRef.current,
+    DocumentTitle: "Product Report",
+    onafterprint: () => alert("Product Report Sccessfully Download !!"),
+  });
+
   return (
-    <div className="inv-Allproducts-page container">
-      <div className="pti-text-dark pti-text-h1 pti-bold text-center pb-5 pt-3">INVENTORY DASHBOARD</div>
+    <div className="inv-Allproducts-page container" ref={ComponentsRef}>
+      <div className="pti-text-dark pti-text-h1 pti-bold text-center pb-5 pt-3">
+        INVENTORY DASHBOARD
+      </div>
       <div className="d-flex justify-content-center gap-3 pb-5">
-        <button className="inv-allProducts-button pti-bg-secondary_blue  pti-bold pti-rounded-small border-0 text-light pti-text-p"><Link className="nav-link active" to="/">PRODUCTS</Link></button>
-        <button to="/add" className="inv-allProducts-button pti-bg-secondary_blue  pti-bold pti-rounded-small border-0 text-light pti-text-p"><Link className="nav-link active" to="/material">MATERIALS</Link></button>
-        <button className="inv-allProducts-button pti-bg-secondary_blue  pti-bold pti-rounded-small border-0 text-light pti-text-p">RELEASED</button>
+        <button className="inv-allProducts-button pti-bg-secondary_blue  pti-bold pti-rounded-small border-0 text-light pti-text-p">
+          <Link className="nav-link active" to="/">
+            PRODUCTS
+          </Link>
+        </button>
+        <button
+          to="/add"
+          className="inv-allProducts-button pti-bg-secondary_blue  pti-bold pti-rounded-small border-0 text-light pti-text-p"
+        >
+          <Link className="nav-link active" to="/material">
+            MATERIALS
+          </Link>
+        </button>
+        <button className="inv-allProducts-button pti-bg-secondary_blue  pti-bold pti-rounded-small border-0 text-light pti-text-p">
+          RELEASED
+        </button>
       </div>
       <div className="d-flex justify-content-between">
         <div className="pti-text-h2 pti-bold text-black">MATERIALS</div>
-        <div>
-          <button className="add-product-btn pti-bg-black p-2 text-light pti-rounded-small pti-bold"><Link className="nav-link active" to="/material/add">ADD MATERIAL</Link></button>
+        <div className="d-flex gap-3"> 
+          <button className="add-product-btn pti-bg-black p-2 text-light pti-rounded-small pti-bold">
+            <Link className="nav-link active" to="/material/add">
+              Add Material
+            </Link>
+          </button>
+          <button
+            className="add-product-btn pti-bg-black p-2 text-light pti-rounded-small pti-bold"
+            onClick={handlePrint}
+          >
+            Download Report
+          </button>
         </div>
       </div>
       <table border={0} className="inv-Allproducts-table">
@@ -54,23 +87,39 @@ export default function AllMaterials() {
           <th>Material type</th>
           <th>Roll Quantity</th>
           <th>Material colour</th>
-          
+
           <th></th>
           <th className="inv-Allproducts-table-heading2"></th>
         </tr>
-          {materials.map((material) => (
-            <tr className=" pti-bg-light_blue ">
-              <td className="inv-Allproducts-table-row1 p-2">{material.material_ID }</td>
-              <td>{material.material_name}</td>
-              <td>{material.material_type}</td>
-              <td>{material.roll_quantity }</td>
-              <td>{material.color }</td>
-              
+        {materials.map((material) => (
+          <tr className=" pti-bg-light_blue ">
+            <td className="inv-Allproducts-table-row1 p-2">
+              {material.material_ID}
+            </td>
+            <td>{material.material_name}</td>
+            <td>{material.material_type}</td>
+            <td>{material.roll_quantity}</td>
+            <td>{material.color}</td>
 
-              <td > <Link to={`/material/${material._id}`}><button className="pti-allProducts-tble-buttons pti-allProducts-edit-buttons"> <i class="fa-solid fa-pen-to-square"></i></button></Link></td>
-              <td  className="inv-Allproducts-table-row2"><button className="pti-allProducts-tble-buttons pti-allProducts-delete-buttons" onClick={() => handleDelete(material._id)}><i class="fa-solid fa-trash"></i></button></td>
-            </tr>
-          ))}
+            <td>
+              {" "}
+              <Link to={`/material/${material._id}`}>
+                <button className="pti-allProducts-tble-buttons pti-allProducts-edit-buttons">
+                  {" "}
+                  <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+              </Link>
+            </td>
+            <td className="inv-Allproducts-table-row2">
+              <button
+                className="pti-allProducts-tble-buttons pti-allProducts-delete-buttons"
+                onClick={() => handleDelete(material._id)}
+              >
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+        ))}
       </table>
     </div>
   );
--- frontend/src/components/AllProducts.js
+++ frontend/src/components/AllProducts.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useReactToPrint } from "react-to-print";
 
 export default function AllProducts() {
   const [products, setProducts] = useState([]);
@@ -32,9 +33,16 @@ export default function AllProducts() {
     }
   };
 
+  const ComponentsRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => ComponentsRef.current,
+    DocumentTitle: "Product Report",
+    onafterprint: () => alert("Product Report Sccessfully Download !!"),
+  });
+
   return (
-    <div className="inv-Allproducts-page container">
-      <div className="pti-text-dark pti-text-h1 pti-bold text-center pb-5 pt-3">
+    <div ref={ComponentsRef} className="inv-Allproducts-page container">
+      <div  className="pti-text-dark pti-text-h1 pti-bold text-center pb-5 pt-3">
         INVENTORY DASHBOARD
       </div>
       <div className="d-flex justify-content-center gap-3 pb-5">
@@ -52,58 +60,66 @@ export default function AllProducts() {
       </div>
       <div className="d-flex justify-content-between">
         <div className="pti-text-h2 pti-bold text-black">PRODUCTS</div>
-        <div>
+        <div className="d-flex gap-3">
           <button className="add-product-btn pti-bg-black p-2 text-light pti-rounded-small pti-bold">
             <Link className="nav-link active" to="/add">
-              ADD PRODUCT
+              Add Product
             </Link>
+          </button>
+          <button
+            className="add-product-btn pti-bg-black p-2 text-light pti-rounded-small pti-bold"
+            onClick={handlePrint}
+          >
+            Download Report
           </button>
         </div>
       </div>
-      <table border={0} className="inv-Allproducts-table">
-        <tr className="inv-Allproducts-table-row pti-bg-secondary_blue text-light  rounded-5">
-          <th className="inv-Allproducts-table-heading1 p-2">Product ID</th>
-          <th>Product Image</th>
-          <th>Product Name</th>
-          <th>Product Description</th>
-          <th>Product Quantity</th>
-          <th>Product Weight</th>
-          <th>Unit Price</th>
-          <th>Size</th>
-          <th></th>
-          <th className="inv-Allproducts-table-heading2"></th>
-        </tr>
-        {products.map((product) => (
-          <tr className=" pti-bg-light_blue ">
-            <td className="inv-Allproducts-table-row1 p-2">
-              {product.product_ID}
-            </td>
-            <td>
-              <img src={product.image} width={"200px"} height={"180px"} />
-            </td>
-            <td>{product.product_name}</td>
-            <td>{product.product_description}</td>
-            <td>{product.quantity}</td>
-            <td>{product.weight}</td>
-            <td>{product.unit_price}</td>
-            <td>{product.size}</td>
-
-            <td>
-              <button className="pti-allProducts-tble-buttons pti-allProducts-edit-buttons">
-                <i class="fa-solid fa-pen-to-square"></i>
-              </button>
-            </td>
-            <td className="inv-Allproducts-table-row2">
-              <button
-                className="pti-allProducts-tble-buttons pti-allProducts-delete-buttons"
-                onClick={() => handleDelete(product._id)}
-              >
-                <i className="fa-solid fa-trash"></i>
-              </button>
-            </td>
+      <div ref={ComponentsRef}>
+        <table border={0} className="inv-Allproducts-table">
+          <tr className="inv-Allproducts-table-row pti-bg-secondary_blue text-light  rounded-5">
+            <th className="inv-Allproducts-table-heading1 p-2">Product ID</th>
+            <th>Product Image</th>
+            <th>Product Name</th>
+            <th>Product Description</th>
+            <th>Product Quantity</th>
+            <th>Product Weight</th>
+            <th>Unit Price</th>
+            <th>Size</th>
+            <th></th>
+            <th className="inv-Allproducts-table-heading2"></th>
           </tr>
-        ))}
-      </table>
+          {products.map((product) => (
+            <tr className=" pti-bg-light_blue ">
+              <td className="inv-Allproducts-table-row1 p-2">
+                {product.product_ID}
+              </td>
+              <td>
+                <img src={product.image} width={"200px"} height={"180px"} />
+              </td>
+              <td>{product.product_name}</td>
+              <td>{product.product_description}</td>
+              <td>{product.quantity}</td>
+              <td>{product.weight}</td>
+              <td>{product.unit_price}</td>
+              <td>{product.size}</td>
+
+              <td>
+                <button className="pti-allProducts-tble-buttons pti-allProducts-edit-buttons">
+                  <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+              </td>
+              <td className="inv-Allproducts-table-row2">
+                <button
+                  className="pti-allProducts-tble-buttons pti-allProducts-delete-buttons"
+                  onClick={() => handleDelete(product._id)}
+                >
+                  <i className="fa-solid fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }

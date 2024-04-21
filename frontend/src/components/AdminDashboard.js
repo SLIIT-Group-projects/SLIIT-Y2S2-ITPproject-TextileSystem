b09ab67d3e4805c7dce15be3634bf54087eefa70
Siluni Rupasinghe <silunirupasinghe@gmail.com>
2024-04-21 12:58:47 +0530

    admin dashborad

---
+++ frontend/src/components/AdminDashboard.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CreateDeliveryLog from "./CreateDeliveryLog";
+import DeliveryHeader from "./DeliveryHeader";
+import '../css/delivery.css'
+
+export default function AdminDashboard() {
+  
+  return (
+    <div className="container">
+        <div>
+            d
+        </div>
+        <div>
+            
+        </div>
+     
+    </div>
+  );
+}

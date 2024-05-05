e9e0ecd359e6faee62f7e5a2f6c763810be2ac37
Siluni Rupasinghe <silunirupasinghe@gmail.com>
2024-05-05 13:42:02 +0530

    mainHeader

--- user management system/client/src/components/MainHeader.jsx
+++ user management system/client/src/components/MainHeader.jsx
@@ -1,11 +1,31 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useReactToPrint } from "react-to-print";
+import { useSelector, useDispatch } from "react-redux";
 
 import '../css/header.css'
 import '../css/main.css'
 export default function Header() {
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signOut());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div>
       <nav class="navbar navbar-expand-lg  pti-bg-light_blue">
@@ -70,12 +90,38 @@ export default function Header() {
                 </a>
               </li>
             </ul>
-          </div>
-          <div>
+            <ul className="d-flex gap-4 align-items-center">
+          
+          {currentUser ? (
+            
+            <div className="dropdown">
+              <Link to='/ApprovedFeedbacks'>
+            <button className="pti-header-signin pti-bg-blue">Feedbacks</button>
+            </Link>
+              <Link to="/profile" className="dropdown-toggle text-decoration-none text-white">
+                <img src={currentUser.profilePicture} alt="user" className="rounded-circle" style={{ width: '3rem', height: '3rem' }} />
+              </Link>
+              <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                <li>
+                  <span className="dropdown-item-text">
+                    <span className="d-block text-sm">@{currentUser.username}</span>
+                    <span className="d-block text-sm font-weight-medium overflow-hidden text-truncate">{currentUser.email}</span>
+                  </span>
+                </li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><button className="dropdown-item text-dark" onClick={handleSignOut}>Sign Out</button></li>
+              </ul>
+            </div>
+          ) : (
             <Link to='/sign-in'>
             <button className="pti-header-signin pti-bg-blue">Sign In</button>
             </Link>
             
+          )}
+        </ul>
+          </div>
+          <div>
+           
           </div>
         </div>
 
